261525c7df1c44c90f082cc76678e18cedbd85ca
Richard laptop <rich.290401@gmail.com>
2022-09-05 13:42:11 +0200

    Added fab commands. Set up settings example

---
+++ config/settings_example.py
@@ -0,0 +1,24 @@
+from settings import *
+
+DEBUG = True
+INSECURE = False
+API_DOCUMENTATION = True
+DEBUG_TOOLBAR = True
+
+TEMPLATES[0]['OPTIONS']['debug'] = True
+
+SECRET_KEY = '<YOUR KEY>'
+
+DATABASES['default']['USER'] = '<DB USER NAME>'
+DATABASES['default']['PASSWORD'] = '<DB USER PASSWORD>'
+
+# url options
+SITE_URL = 'joker.local'
+SITE_SCHEME = "http"
+PARENT_HOST = ".%s" % SITE_URL
+HOST_PORT = '1111'
+SITE = "%s://%s:%s" % (SITE_SCHEME, SITE_URL, HOST_PORT)
+
+if DEBUG_TOOLBAR:
+    INSTALLED_APPS += ['debug_toolbar']
+    MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
--- settings.py
+++ settings.py
@@ -9,12 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
-from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__))) + '/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -23,10 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tl2-=0-@b*e)xmg9z-u%f123ltjt8btz#7mwmmt#f$(ha^fqfg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+INSECURE = False
+API_DOCUMENTATION = True
+DEBUG_TOOLBAR = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
+SITE_URL = 'joker'
+SITE_SCHEME = "http"
+PARENT_HOST = ".%s" % SITE_URL
+HOST_PORT = None
+SITE = "%s://%s:%s" % (SITE_SCHEME, SITE_URL, HOST_PORT)
 
 # Application definition
 
@@ -37,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -69,17 +76,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jokerdb',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'ATOMIC_REQUESTS': True,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -99,27 +109,29 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
+MEDIA_URL = '/media/'
+MEDIA__ROOT = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPH_MODELS = {
+    'all_applications': True,
+    'group_models': ["auth"],
+}

62182056fbbb895221ed7e1baea7f81cc64d87aa
Richard laptop <rich.290401@gmail.com>
2022-09-09 18:03:58 +0200

    Removed from table unused field in privilege messages

--- Admin/Privileges/PrivilegeMessages/tables.py
+++ Admin/Privileges/PrivilegeMessages/tables.py
@@ -5,11 +5,10 @@ from core.Privilege.models import PrivilegeMessage
 class PrivilegeMessagesTable(tables.Table):
     message = tables.TemplateColumn(template_name='Admin/PrivilegeUser/PrivilegeMessage/privilege_message_message_label.html', # noqa
                                     orderable=False)
-    is_active = tables.BooleanColumn(orderable=False)
     actions = tables.TemplateColumn(template_name='Admin/PrivilegeUser/PrivilegeMessage/privilege_message_actions.html',  # noqa
                                     orderable=False)
 
     class Meta:
         model = PrivilegeMessage
-        fields = ('pk', 'message', 'is_active', 'actions')
+        fields = ('pk', 'message', 'actions')
         template_name = "django_tables2/bootstrap4.html"

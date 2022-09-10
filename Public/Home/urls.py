0109749d6add974e327ebe3b79d893d3954be915
Richard laptop <rich.290401@gmail.com>
2022-09-10 19:32:25 +0200

    Added basic public pages Added public views Added public HTMLs

--- Public/Home/urls.py
+++ Public/Home/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^$', views.home_view, name='home-index')
+    url('^$', views.home_view, name='home-index'),
+    url('^all-jokes/$', views.home_all_jokes_view, name='home-all-jokes'),
+    url('^top-jokes/$', views.home_top_jokes_view, name='home-top-jokes'),
 ]
--- Public/Home/views.py
+++ Public/Home/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.db import models
+from core.Joke.models import Joke
 
 
 def home_view(request):
-    return render(request, 'Public/public_home.html')
+    return render(request, 'Public/Home/public_home.html')
+
+
+def home_all_jokes_view(request):
+    jokes = Joke.objects.active().ordered()
+    return render(request, 'Public/Home/public_all_jokes.html', {'jokes': jokes})
+
+
+def home_top_jokes_view(request):
+    jokes = Joke.objects.prefetch_related('jokelikestatus_set').active().all()
+    jokes = jokes.annotate(likes=models.Count('jokelikestatus'))
+    jokes = jokes.filter(~models.Q(likes=0))
+    jokes = jokes.order_by('likes')
+    return render(request, 'Public/Home/public_top_jokes.html', {'jokes': jokes})

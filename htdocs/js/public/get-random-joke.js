1adc3647f8c00f19373c40ea0277bc0b08a95ff5
richard <rich.290401@gmail.com>
2022-09-16 22:21:27 +0200

    Commit

--- htdocs/js/public/get-random-joke.js
+++ htdocs/js/public/get-random-joke.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
         event.preventDefault();
 
         let sessionKey = 'seenJokes'
-
         let seenJokes = localStorage.getItem(sessionKey) || [];
         $.ajax({
             url: $(this).data('url'),
@@ -12,28 +11,19 @@ $(document).ready(function () {
             success: function (data, status, xhr) {
                 console.log(localStorage.getItem(sessionKey))
                 if (!localStorage.getItem(sessionKey)) {
-                    console.log('init me')
                     let empty = [];
                     localStorage.setItem(sessionKey, empty);
                 }
-
                 if ('add_to_cache' in data) {
-                    console.log('add data to cache');
                     let jokeId = data['id'].toString();
                     let seen_jokes = localStorage.getItem(sessionKey);
-                    console.log('seen_jokes', seen_jokes)
-                    console.log('jokeId', jokeId)
                     seen_jokes.push(jokeId);
                     localStorage.setItem(sessionKey, seen_jokes);
                 }
-
                 if ('all_jokes_seen' in data) {
                     console.log('clear all jokes');
                     localStorage.clear();
                 }
-
-                console.log(data)
-                console.log(localStorage.getItem(sessionKey))
             },
             error: function (jqXhr, textStatus, errorMessage) {
                 alert(errorMessage);

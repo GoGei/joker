53f27b7b6c9f3b5920715846567df2b0811550fa
richard <rich.290401@gmail.com>
2022-09-25 18:22:15 +0200

    - Added basic js to handle get random function

--- htdocs/js/public/get-random-joke.js
+++ htdocs/js/public/get-random-joke.js
@@ -2,27 +2,14 @@ $(document).ready(function () {
     $('#getRandomButton').click(function (event) {
         event.preventDefault();
 
-        let sessionKey = 'seenJokes'
-        let seenJokes = localStorage.getItem(sessionKey) || [];
         $.ajax({
             url: $(this).data('url'),
             method: 'get',
-            data: {'seen_jokes': seenJokes},
             success: function (data, status, xhr) {
-                console.log(localStorage.getItem(sessionKey))
-                if (!localStorage.getItem(sessionKey)) {
-                    let empty = [];
-                    localStorage.setItem(sessionKey, empty);
-                }
-                if ('add_to_cache' in data) {
-                    let jokeId = data['id'].toString();
-                    let seen_jokes = localStorage.getItem(sessionKey);
-                    seen_jokes.push(jokeId);
-                    localStorage.setItem(sessionKey, seen_jokes);
-                }
                 if ('all_jokes_seen' in data) {
-                    console.log('clear all jokes');
-                    localStorage.clear();
+                    console.log('All seen')
+                } else {
+                    console.log(data)
                 }
             },
             error: function (jqXhr, textStatus, errorMessage) {
@@ -30,4 +17,4 @@ $(document).ready(function () {
             }
         })
     })
-})
+});
